13db03ccbe057991cb5585129c23f588a2da7152
BlackHumbleCow <gunhee0218@naver.com>
2025-05-22 17:16:29 +0900

    [EDIT] Movement, StudyGameObject
    Destroy 추가 포함

--- Assets/02. Scripts/Movement.cs
+++ Assets/02. Scripts/Movement.cs
@@ -14,25 +14,43 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-        if (Input.GetKey(KeyCode.W)) // 앞으로 가는 기능
-        {
-            transform.position += Vector3.forward * Time.deltaTime * MoveSpeed;
-        }
-        
-        if (Input.GetKey(KeyCode.S)) // 뒤로 가는 기능
-        {
-            transform.position += Vector3.back * Time.deltaTime * MoveSpeed;
-        }
-        
-        if (Input.GetKey(KeyCode.A)) // 왼쪽으로 가는 기능
-        {
-            transform.position += Vector3.left * Time.deltaTime * MoveSpeed;
-        }
-        
-        if (Input.GetKey(KeyCode.D)) // 오른쪽으로 가는 기능
-        {
-            transform.position += Vector3.right * Time.deltaTime * MoveSpeed;
-        }
+        /// Input System(old - Legacy)
+        /// 입력값에 대한 약속된 시스템
+        /// 이동 - WASD, 화살표 키보드
+        /// 점프 - Space
+        /// 총 쏘기 - 마우스 좌클릭
+
+        //if (Input.GetKey(KeyCode.W)) // 앞으로 가는 기능
+        //{
+        //    transform.position += Vector3.forward * Time.deltaTime * MoveSpeed;
+        //}
+
+        //if (Input.GetKey(KeyCode.S)) // 뒤로 가는 기능
+        //{
+        //    transform.position += Vector3.back * Time.deltaTime * MoveSpeed;
+        //}
+
+        //if (Input.GetKey(KeyCode.A)) // 왼쪽으로 가는 기능
+        //{
+        //    transform.position += Vector3.left * Time.deltaTime * MoveSpeed;
+        //}
+
+        //if (Input.GetKey(KeyCode.D)) // 오른쪽으로 가는 기능
+        //{
+        //    transform.position += Vector3.right * Time.deltaTime * MoveSpeed;
+        //}
+
+        //부드럽게 증감하는 값
+        float h = Input.GetAxis("Horizontal"); // x값 조작. 왼쪽은 -x값 오른쪽은 +값
+        float v = Input.GetAxis("Vertical"); //y값 조작. 위쪽은 +y값 아래쪽은 -y값
+
+        //1씩 딱 떨어지는 값 1 아니면 -1
+        //float hr = Input.GetAxisRaw("Horizontal"); // x값 조작. 왼쪽은 -x값 오른쪽은 +값
+        //float vr = Input.GetAxisRaw("Vertical"); //y값 조작. 위쪽은 +y값 아래쪽은 -y값
+
+        Vector3 dir = new Vector3(h, 0, v);
+        Debug.Log($"현재 입력: {dir}");
+
+        transform.position += dir * MoveSpeed * Time.deltaTime;
     }
 }
--- Assets/02. Scripts/StudyGameObject.cs
+++ Assets/02. Scripts/StudyGameObject.cs
@@ -1,13 +1,47 @@
+using System;
 using UnityEngine;
 
 public class StudyGameObject : MonoBehaviour
 {
     public GameObject prefab;
 
+    //public GameObject destroyObj;
+
+    //public Vector3 pos; // 위치 x,y,z 0 0 0
+    //public Quaternion rot; // 회전 0,0,0,0
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void Start() //유니티 에디터 실행시 1번 작동
     {
-        Instantiate(prefab);
+        CreateAmogus();
+
+        //Destroy(destroyObj, 3f); // 지정한 게임오브젝트 3초 뒤 파괴 
+    }
+
+    //void OnDestroy() // Spawner가 파괴될 때 작동
+    //{
+    //    Debug.Log("파괴되었습니다.");
+    //}
+
+    //어몽어스 캐릭터를 생성하고 이름을 부여하는 기능
+    public void CreateAmogus()
+    {
+        GameObject obj = Instantiate(prefab);
+        obj.name = "AMOGUS SUS";
+
+        Transform objTf = obj.transform;
+        //자식 방향으로 접근
+        int countObj = objTf.childCount; 
+
+        //부모 방향으로 접근
+        //objTf.parent;
+        //objTf.root;
+
+        Debug.Log($"캐릭터의 자식 오브젝트 수: {countObj}"); //자식 오브젝트의 수 접근은 transform을 통해
+        
+        Debug.Log($"캐릭터의 첫번째 자식 오브젝트 수: {obj.transform.GetChild(0).name}"); //첫번째 자식 오브젝트 이름 접근
+
+        Debug.Log($"캐릭터의 마지막 자식 오브젝트 수: {obj.transform.GetChild(countObj).name}"); //마지막 자식 오브젝트 이름 접근
     }
 
 }
